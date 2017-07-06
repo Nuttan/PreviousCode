527ffe1ef3b45aa05408a1d1b9778c0af47a2906
nuttan swain <nuttanabhijan.swain@utc.com>
2017-07-06 11:43:44 +0530

    initial commit

---
+++ LatestSelenium/src/ObjectRepository/ObjectMap.java
@@ -0,0 +1,15 @@
+package ObjectRepository;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+@SuppressWarnings("hiding")
+public class ObjectMap<String, UIControlObject> extends ConcurrentHashMap<String, UIControlObject> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2794493072205607603L;
+
+}
