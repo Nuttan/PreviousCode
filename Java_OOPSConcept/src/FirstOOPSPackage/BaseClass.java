130d49b6f4c8127310ab9cb8e2fad223a24c618e
Nuttan <eng.nuttan@gmail.com>
2017-07-28 01:33:05 +0530

    +Commited first time java prog
    Signed-off-by: Nuttan <eng.nuttan@gmail.com>

---
+++ Java_OOPSConcept/src/FirstOOPSPackage/BaseClass.java
@@ -0,0 +1,15 @@
+package FirstOOPSPackage;
+
+public class BaseClass {
+	
+	BaseClass ()
+    {
+        System.out.println("Base Class Constructor executed");
+    }
+                                 
+    public void Write ()
+    {
+    	System.out.println("Write method in Base Class executed");
+    }
+
+}
